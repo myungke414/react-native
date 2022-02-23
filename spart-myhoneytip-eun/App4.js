78ef9d55537fc81e15439e9541d3f7d240b1d0d7
myungke414 <74960705+myungke414@users.noreply.github.com>
2022-02-23 22:48:18 +0900

    Update App4.js

--- spart-myhoneytip-eun/App4.js
+++ spart-myhoneytip-eun/App4.js
@@ -7,11 +7,11 @@ import favicon from "./assets/favicon.png"
 export default function App() {
   return (
     <View style={styles.container}>
-			{/*이미지 태그 soruce 부분에 가져온 미지 이름을 넣습니다 */}
+      {/*이미지 태그 soruce 부분에 가져온 미지 이름을 넣습니다 */}
       <Image 
         //source={favicon}
         source={{uri:'https://images.unsplash.com/photo-1424819827928-55f0c8497861?fit=crop&w=600&h=600%27'}}
-				// 사용설명서에 나와 있는 resizeMode 속성 값을 그대로 넣어 적용합니다
+	// 사용설명서에 나와 있는 resizeMode 속성 값을 그대로 넣어 적용합니다
         resizeMode={"cover"} //repeat, contain
         style={styles.imageStyle}
       />
